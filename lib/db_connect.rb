d4751d5b67c74bac84afb92a213a79633aafdd77
Chris Casola <Chris@dean-street-141-214.dyn.wpi.edu>
2012-03-31 17:07:39 -0400

    .

--- lib/db_connect.rb
+++ lib/db_connect.rb
@@ -7,5 +7,28 @@ require 'pg'
 def connectToDB(dbPath)
   conn = PG::Connection.new('ec2-23-21-132-139.compute-1.amazonaws.com', nil, nil, nil, 'hblzjagplr', 'hblzjagplr', 'L4cX88lAA9DU2vB5Szxy')
   
-  conn.exec('SELECT * FROM randomtblname')
+  queryStr1 = <<EOS
+    CREATE TABLE test_table
+    (
+      userid integer,
+      username varchar(20)
+    )
+EOS
+
+  queryStr2 = <<EOS
+    INSERT INTO test_table
+    VALUES (1, 'bob')
+    
+    INSERT INTO test_table
+    VALUES (2, 'chris')
+EOS
+  
+  queryStr3 = <<EOS
+    SELECT * FROM test_table
+EOS
+  
+  conn.exec(queryStr1)
+  conn.exec(queryStr2)
+  results = conn.exec(queryStr3)
+  'success'
 end
