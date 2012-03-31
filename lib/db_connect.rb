9e2413936e7cae3ee358fe2c5fed450fdb3093f3
Chris Casola <Chris@dean-street-141-214.dyn.wpi.edu>
2012-03-31 16:59:47 -0400

    .

--- lib/db_connect.rb
+++ lib/db_connect.rb
@@ -5,5 +5,7 @@
 require 'pg'
 
 def connectToDB(dbPath)
-  PG::Connection.new('ec2-23-21-132-139.compute-1.amazonaws.com', nil, nil, nil, 'hblzjagplr', 'hblzjagplr', 'L4cX88lAA9DU2vB5Szxy')
+  conn = PG::Connection.new('ec2-23-21-132-139.compute-1.amazonaws.com', nil, nil, nil, 'hblzjagplr', 'hblzjagplr', 'L4cX88lAA9DU2vB5Szxy')
+  
+  conn.exec('SELECT * FROM randomtblname')
 end
