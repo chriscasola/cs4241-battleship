c882a8b4c080280483be5c9a30c4b12a527d9760
Chris Casola <Chris@dean-street-141-214.dyn.wpi.edu>
2012-03-31 17:53:44 -0400

    .

--- lib/db_connect.rb
+++ lib/db_connect.rb
@@ -5,27 +5,19 @@
 require 'pg'
 
 def connectToDB(dbPath)
-  conn = PG::Connection.new('ec2-23-21-132-139.compute-1.amazonaws.com', nil, nil, nil, 'hblzjagplr', 'hblzjagplr', 'L4cX88lAA9DU2vB5Szxy')
   
-  queryStr1 = <<EOS
-    CREATE TABLE test_table
-    (
-      userid integer,
-      username varchar(20)
-    );
-EOS
-
-  queryStr2 = <<EOS
-    INSERT INTO test_table VALUES (1, 'bob');
-    INSERT INTO test_table VALUES (2, 'chris');
-EOS
+  dbPath =~ /^postgres:\/\/(\S*):(\S*)@(\S*)\/(\S*)$/
+  
+  conn = PG::Connection.new($3, nil, nil, nil, $1, $4, $2)
+  
+  #conn = PG::Connection.new('ec2-23-21-132-139.compute-1.amazonaws.com', nil, nil, nil, 'hblzjagplr', 'hblzjagplr', 'L4cX88lAA9DU2vB5Szxy')
+  
+  #conn = PG::Connection.new('localhost', nil, nil, nil, 'battledb', 'Chris', '3445')
   
   queryStr3 = <<EOS
     SELECT * FROM test_table;
 EOS
   
-  conn.exec(queryStr1)
-  conn.exec(queryStr2)
   results = conn.exec(queryStr3)
   'success'
 end
