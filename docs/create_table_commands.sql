e721bf2605037b360f0c505c2c6731c5b784570c
Jennifer Page <jeni@awesome-l.(none)>
2012-04-04 09:34:14 -0400

    Updated comments.

--- docs/create_table_commands.sql
+++ docs/create_table_commands.sql
@@ -1,18 +1,32 @@
 
 /**
- * The users table keeps track of information important for login and 
- * management of a user account.
+ * Keeps track of information important for login and management of a user 
+ * account.
+ *
+ * Columns:
+ *   userid		This is an automatically generated id for the user.
+ *   name		The name of the user.
+ *   email		The email which is used to log in and for password resets.
+ *   password	The hashed password of the user. NOTE: We should implement a 
+ *				salting system as well.
  */
 CREATE TABLE users (
 	userid integer PRIMARY KEY,
 	name varchar NOT NULL UNIQUE,
-	email varchar,
+	email varchar NOT NULL UNIQUE,
 	password varchar NOT NULL,
 );
 
 /**
- * The users_online table is for keeping track of users who are currently 
- * logged in.
+ * Keeps track of users who are currently logged in.
+ *
+ * Columns:
+ *   sessionid	The session id of the logged on user.
+ *   userid		The id of a specific logged on user.
+ *   timein		When the user logged on. This column's value is set when the 
+ 				user logs in.
+ *   laston		When the user last accessed the website while logged in. This 
+ *				field must be updated every time the user performs an action.
  */
 CREATE TABLE users_online (
 	sessionid varchar PRIMARY KEY,
@@ -22,7 +36,9 @@ CREATE TABLE users_online (
 );
 
 /**
- * The users_icons table keeps track of what icon each user has.
+ * The users_icons table keeps track of what icon each user has. An entry for 
+ * a user does not have to be created when the user is created. If there is no 
+ * row for a specific userid, then the user has no icon.
  */
 CREATE TABLE users_icons (
 	userid integer PRIMARY KEY REFERENCES users(userid),
@@ -30,17 +46,21 @@ CREATE TABLE users_icons (
 );
 
 /**
- * The users_waiting_for_battle table keeps track of users who are waiting for 
- * a battle opponent.
+ * Keeps track of users who are waiting for a battle opponent.
  */
 CREATE TABLE users_waiting_for_battle (
 	userid integer PRIMARY KEY REFERENCES users(userid),
 	startedWaiting timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
+/**
+ * An enum for identifying the status of a battle.
+ */
 CREATE TYPE battlestatus AS ENUM ('p1turn', 'p2turn', 'p1win', 'p2win', 'tie', 'endofworld');
 
 /**
+ * Keeps track of individual battles.
+ *
  * Use a special SELECT statement to find the number of wins and losses a user has.
  */
 CREATE TABLE battles (
@@ -53,10 +73,29 @@ CREATE TABLE battles (
 	status battlestatus DEFAULT 'p1turn'
 );
 
+/**
+ * An enum for identifying a type of ship.
+ */
 CREATE TYPE shiptype AS ENUM ('carrier', 'battleship', 'submarine', 'cruiser', 'destroyer');
 
+/**
+ * An enum for identifying whether or a ship is placed horizontally or 
+ * vertically.
+ */
 CREATE TYPE orientation AS ENUM ('horizontal', 'vertical');
 
+/**
+ * Keeps track of a user's ship positions on the field of a particular battle.
+ *
+ * Columns:
+ *   battleid		The id of the battle in which the ship is taking part.
+ *   playerid		The id of the player who owns the ship.
+ *   xpos			The x location of the leftmost peg of the ship.
+ *   ypos			The y location of the uppermost peg of the ship.
+ *   stype			They type of ship that is being placed.
+ *   orientation	Whether or not the ship is horizontal or vertical.
+ *   afloat			Whether or not the ship is afloat.
+ */
 CREATE TABLE battle_positions (
 	battleid integer REFERENCES battles(battleid) ON DELETE CASCADE,
 	playerid integer REFERENCES users(userid),
@@ -68,6 +107,9 @@ CREATE TABLE battle_positions (
 	CONSTRAINT pk_ships PRIMARY KEY (battleid, playerid, xpos, ypos)
 );
 
+/**
+ * Keeps track of a user's moves in a particular battle.
+ */
 CREATE TABLE battle_moves (
 	battleid integer REFERENCES battles(battleid) ON DELETE CASCADE,
 	playerid integer REFERENCES users(userid),
