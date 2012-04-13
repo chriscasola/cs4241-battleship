dbc4dd42a81a442f371e0d077d4b34a7f69703b2
Chris Casola <Chris@chriscasola-macbookpro.wifi.wpi.edu>
2012-04-12 20:14:56 -0400

    Fix indentation.

--- lib/dbmgr.rb
+++ lib/dbmgr.rb
@@ -5,12 +5,12 @@
 require 'pg'
 
 def connectToDB(dbPath)
-  dbPath =~ %r|^postgres://(\S*):(\S*)@(\S*)/(\S*)$|
-  conn = PG::Connection.new( :host => $3, :dbname => $1, :user => $4, :password => $2)
+    dbPath =~ %r|^postgres://(\S*):(\S*)@(\S*)/(\S*)$|
+    conn = PG::Connection.new( :host => $3, :dbname => $1, :user => $4, :password => $2)
 end
 
 def runDBShell (dbPath, params=nil)
-  if (params == nil)
+    if (params == nil)
 <<EOS
   <!DOCTYPE html>
   <html>
@@ -27,15 +27,15 @@ def runDBShell (dbPath, params=nil)
     </body>
   </html>
 EOS
-  else
-    conn = connectToDB(dbPath)
-    results = conn.exec(params[:sqlCode])
-    strOut = ''
-    results.each do |row|
-      row.each do |column|
-        strOut = strOut + ' ' + column.to_s
-      end
+    else
+        conn = connectToDB(dbPath)
+        results = conn.exec(params[:sqlCode])
+        strOut = ''
+        results.each do |row|
+            row.each do |column|
+                strOut = strOut + ' ' + column.to_s
+            end
+        end
+        strOut + '<br /><br /><br /><a href="/db_manager">Enter another query</a>'
     end
-    strOut + '<br /><br /><br /><a href="/db_manager">Enter another query</a>'
-  end
 end
