fd0c62dec91c354a281344103a9658bc2e5ea518
Jennifer Page <jeni@awesome-l.(none)>
2012-04-13 21:39:49 -0400

    Transitioned login.rb and simple_server.rb to a modular style Sinatra application.

--- simple_server.rb
+++ simple_server.rb
@@ -1,66 +1,76 @@
-##
-# This is a simple Web server, mainly for serving static content with some JavaScript in order
-# to get started building a Web site.
-#
-# Chris Casola
-# Chris Page
-##
+=begin
+  # TODO This comment
+  
+  @author Chris Casola
+  @author Chris Page
+  @version 4/13/2012
+=end
+
+# http://www.sinatrarb.com/intro.html
+# http://www.sinatrarb.com/extensions.html
+# http://stackoverflow.com/questions/5015471/using-sinatra-for-larger-projects-via-multiple-files <-- forget this for now
 
 # Add the lib directory to the search path
 $: << File.expand_path(File.dirname(__FILE__) + "/lib")
 
-require 'sinatra'
+require 'sinatra/base'
 require 'api/dbmgr'
 require 'api/login'
 require 'api/register'
 require 'api/game_play'
 require 'json'
 
-set :public_folder, File.dirname(__FILE__) + '/public'
+#4567
 
-configure do
-  enable :sessions
+ class BattleShip < Sinatra::Base
+ 	set :static, true
+    set :public, File.dirname(__FILE__) + '/public'
+
+    use LoginApi
+
+    #configure do
+    #  enable :sessions
+    #end
+    
+    get '/' do
+        redirect 'http://' + request.host_with_port() + '/index.html'
+    end
+
+    get '/test' do
+        unless (request.env['HTTP_X_FORWARDED_PROTO'] || request.env['rack.url_scheme'])=='https'
+            redirect 'https://' + request.host_with_port() + '/test'
+        end
+        'success!'
+    end
+
+    post '/api/shot' do
+        receive_shot(request.body.read)
+    end
+
+    post '/api/check_shot' do
+        send_shots(request.body.read)
+    end
+
+    post '/api/ship' do
+        receive_ship(request.body.read)
+    end
+
+    post '/api/get_ships' do
+        send_ships(request.body.read)
+    end
+
+    get '/db_manager' do
+        runDBShell(ENV['SHARED_DATABASE_URL'])
+    end
+
+    post '/db_manager' do
+        runDBShell(ENV['SHARED_DATABASE_URL'], params)
+    end
+
+    get '*' do
+        "Path: " + request.fullpath()
+    end
+
 end
 
-get '/' do
-  redirect 'http://' + request.host_with_port() + '/index.html'
-end
-
-get '/test' do
-  unless (request.env['HTTP_X_FORWARDED_PROTO'] || request.env['rack.url_scheme'])=='https'
-    redirect 'https://' + request.host_with_port() + '/test'
-  end
-  'success!'
-end
-
-post '/api/login' do
-  login(params[:email], params[:password])
-end
-
-post '/api/shot' do
-  receive_shot(request.body.read)
-end
-
-post '/api/check_shot' do
-  send_shots(request.body.read)
-end
-
-post '/api/ship' do
-  receive_ship(request.body.read)
-end
-
-post '/api/get_ships' do
-    send_ships(request.body.read)
-end
-
-get '/db_manager' do
-  runDBShell(ENV['SHARED_DATABASE_URL'])
-end
-
-post '/db_manager' do
-  runDBShell(ENV['SHARED_DATABASE_URL'], params)
-end
-
-get '*' do
-  "Path: " + request.fullpath()
-end
+BattleShip.run!
