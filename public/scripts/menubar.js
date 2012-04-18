5f468f9877f436cd19afabd0c0ff94ce49b342c4
Jennifer Page <jeni@awesome-l.(none)>
2012-04-18 16:01:27 -0400

    menubar.js redone

--- public/scripts/menubar.js
+++ public/scripts/menubar.js
@@ -1,20 +1,115 @@
+/**
+ * TODO This comment.
+ * 
+ * @author Chris Casola
+ * @author Chris Page
+ */
+
+var CurrentPage;
+
+var menuBarHTML =	'<nav>' + 
+					'	<ul id="menuBarList">' + 
+					'	</ul>' + 
+					'</nav>';
 
 /*
  * Menu items and their links
  */
 var sectionNames = [
-	{title: "Home", href: "/index.html"},
-	{title: "About", href: "/about.html"},
-	{title: "Leaderboard", href: "/leaderboard.html"},
-	{title: "Battle", href: "/battle.html"},
+	{title: "Home", href: "/index.html", loggedInOnly: false},
+	{title: "About", href: "/about.html", loggedInOnly: false},
+	{title: "Leaderboard", href: "/leaderboard.html", loggedInOnly: false},
+	{title: "MyBattle", href: "/battle.html", loggedInOnly: true}
 ];
 
 var extraButtons = [
 	{title: "Login", href: null, onclick: "showLoginOverlay()"},
-	{title: "Register", href: null, onclick: "showRegisterOverlay()"},
+	{title: "Register", href: null, onclick: "showRegisterOverlay()"}
 ];
 
-function generateTopMenu() {
+function fillTopMenuWithSections(currentPage) {
+	var elMenuBarList = document.getElementById("menuBarList");
+	var i;
+	
+	for (i=0; i < sectionNames.length; i++) {
+		if ((sectionNames[i].loggedInOnly && userIsLoggedIn()) || 
+			!sectionNames[i].loggedInOnly) {
+			var newSectionLi = document.createElement("li");
+			var newSectionLiA = document.createElement("a");
+			var newSectionLiAP = document.createElement("p");
+			
+			newSectionLiAP.innerHTML = sectionNames[i].title;
+			
+			newSectionLiA.appendChild(newSectionLiAP);
+			newSectionLiA.setAttribute("href", sectionNames[i].href);
+			
+			newSectionLi.appendChild(newSectionLiA);
+			
+			if (currentPage == sectionNames[i].title) {
+				newSectionLi.setAttribute("class","activePage");
+			}
+			
+			elMenuBarList.appendChild(newSectionLi);
+		}
+	}
+}
+
+function fillTopMenuRightSide() {
+	if (userIsLoggedIn()) {
+		fillTopMenuRightSideUserStuff();
+	}
+	else {
+		fillTopMenuRightSideButtons();
+	}
+}
+
+function fillTopMenuRightSideUserStuff() {
+	
+}
+
+function fillTopMenuRightSideButtons() {
+	var elMenuBarList = document.getElementById("menuBarList");
+	var i;
+	
+	for (i=0; i < extraButtons.length; i++) {
+		var newSectionLi = document.createElement("li");
+		var newSectionLiA = document.createElement("a");
+		var newSectionLiAP = document.createElement("p");
+		
+		newSectionLiAP.innerHTML = extraButtons[i].title;
+		
+		newSectionLiA.appendChild(newSectionLiAP);
+		
+		if (extraButtons[i].href != null) {
+			newSectionLiA.setAttribute("href", extraButtons[i].href);
+		}
+		if (extraButtons[i].onclick != null) {
+			newSectionLiA.setAttribute("onclick", extraButtons[i].onclick);
+		}
+		
+		newSectionLi.appendChild(newSectionLiA);
+		
+		newSectionLi.setAttribute("class", "rightSide");
+		
+		elMenuBarList.appendChild(newSectionLi);
+	}
+}
+
+function generateTopMenu(currentPage) {
+	CurrentPage = currentPage;
+	document.write(menuBarHTML);
+	
+	fillTopMenuWithSections(currentPage);
+	fillTopMenuRightSide();
+}
+
+function refreshTopMenu() {
+	var elMenuBarList = document.getElementById("menuBarList");
+	
+	//elMenuBarList = 
+}
+
+/*function generateTopMenu() {
 	var menuHTML = '<nav><ul>';
 	var i;
 	var currPage = getCurrPage();
@@ -40,6 +135,10 @@ function generateTopMenu() {
 	}
 	menuHTML += '</ul></nav>'
 	document.write(menuHTML);
+}*/
+
+function userIsLoggedIn() {
+	return true;
 }
 
 function getCurrPage() {
