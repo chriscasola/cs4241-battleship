578945af3f8dc29bb7b9f2931f311fc7d6b142a2
Chris Casola <Chris@chrismbp.local>
2012-04-17 21:18:03 -0400

    Working on CSS and JavaScript for the mybattles table.

--- public/scripts/battles.js
+++ public/scripts/battles.js
@@ -1,16 +1,33 @@
-/*
- * File to display battles a user is involved with
- * 
- * Author: Chris Casola
- * Author: Chris Page
- * 
- */
-
-window.onload = function () {
-	if (sessionStorage['playerid'] == undefined) {
-		document.getElementById('mainContent').innerHTML="<p>You need to login to see your battles.</p>";
-	}
-	else {
-		document.getElementById('mainContent').innerHTML="<h1>My Battles</h1>";
-	}
-}
+// /*
+ // * File to display battles a user is involved with
+ // *
+ // * Author: Chris Casola
+ // * Author: Chris Page
+ // *
+ // */
+// 
+// window.onload = function() {
+	// document.getElementById('mainContent').innerHTML = "<h1>My Battles</h1>";
+// 	
+	// if (sessionStorage['playerid'] == undefined) {
+		// document.getElementById('mainContent').innerHTML += "<p>You need to login to see your battles.</p>";
+	// } 
+	// else {
+		// $.ajax({
+			// type : 'POST',
+			// url : '/api/mybattles',
+			// data : sessionStorage['playerid'],
+			// success : displayBattles,
+			// error : myBattlesReqFail,
+			// dataType : 'text'
+		// });
+	// }
+// }
+// 
+// function displayBattles(response) {
+// 	
+// }
+// 
+// function myBattlesReqFail(error) {
+	// document.getElementById('mainContent').innerHTML += "<p>An error occured retrieving your battles from the server.</p>";
+// }
